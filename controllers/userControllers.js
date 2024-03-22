8a7f11779761558fda20fc6b5bbd8cbbdeef6f18
nv22051-ali-safar <147384419+nv22051-ali-safar@users.noreply.github.com>
2024-03-22 12:29:15 +0000

    15 - custom error handler

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,7 @@
 
 import User from "../models/User";
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
     try{
         const { name, email, password } = req.body;
 
@@ -9,7 +9,8 @@ export const registerUser = async (req, res) => {
         let user = await User.findOne({email});
 
         if(user) {
-            return res.status(400).json({message: "User have already registered"})
+            //return res.status(400).json({message: "User have already registered"});
+            throw new Error("User have already registered")
         }
 
         // creating a new year
@@ -29,7 +30,7 @@ export const registerUser = async (req, res) => {
             token: await user.generateJWT(),
         })
     } catch (error) {
-        return res.status(500).json({message: "Something went wrong! "})
+        next(error);    
     }
 };
 
